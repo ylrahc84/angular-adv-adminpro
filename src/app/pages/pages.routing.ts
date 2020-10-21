66ad7516b0a3dfdfe1495e1d88fa2db00f6a5ae7
charly <coleccionista84@gmail.com>
2020-10-21 07:27:28 -0600

    Fin seccion 4 - Router

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+
+const routes: Routes = [
+ 
+    {
+        path: 'dashboard', //Ruta por defecto
+        component: PagesComponent,
+        children:[
+         { path: '', component: DashboardComponent}, //Path por Defecto al ingresar
+         { path: 'progress', component: ProgressComponent},
+         { path: 'grafica1', component: Grafica1Component},
+         //{ path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+        ] 
+     },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [ RouterModule.forChild(routes)  ],
+  exports: [ RouterModule]
+})
+
+
+export class PageRoutingModule { }
